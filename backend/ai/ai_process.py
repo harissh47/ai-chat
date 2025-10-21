0d8e5902d1128ca9afe3d7e299c529bc42ee8b2f
Harissh <jn.harissh@gmail.com>
2025-10-21 13:18:11 +0530

    half service app

--- backend/ai/ai_process.py
+++ backend/ai/ai_process.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 import os
 from settings.settings import settings
 # client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
+from google import genai
 
 class ai_process():
     def __init__(self):
@@ -17,14 +17,27 @@ class ai_process():
         # self.text=self.scraper.extract_text_only(url)
         
         self.Name,self.API_KEY,self.URL=self.settings.get_settings().values()
+        print(self.Name,self.API_KEY,self.URL)
         
 
     def summary(self,url):
         data = self.scraper.extract_text_only(url)
         print(self.Name)
-        if self.Name!="ollama":
-            print("ollama is not selected")
-        else:
+        if self.Name=="other":
+            print("other is selected")
+        elif self.Name=="google":
+            print("google is selected")
+            client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+            response = client.models.generate_content(
+                model="gemini-2.5-pro",
+                contents=f"""Summarize the following text in a concise report ,
+                            give me a point by point summary , 
+                            give me the recommendations of the stocks to buy or sell  with proper reasoning and based on the following text :
+                            {data}"""
+            )
+            print(response.text)
+            return response.text
+        elif self.Name=="ollama":
             print("ai process is starting with ollama")
             prompt=f"Summarize the following text in a concise report:\n\n{data}"
             print(prompt)
@@ -36,9 +49,10 @@ class ai_process():
             print("result-------------------------------------------------------------------------------")
             print(result.stdout.decode("utf-8"))
             print("ai process is done with ollama")
+            return result.stdout.decode("utf-8")
         # self.settings.set_settings(self.Name,self.API_KEY,self.URL)
         
-        return result.stdout.decode("utf-8")
+        # return result.stdout.decode("utf-8")
         # print(prompt)
         # result=client.chat.completions.create(
         #     model="gpt-4o-mini",
